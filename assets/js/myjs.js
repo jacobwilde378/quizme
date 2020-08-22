17ad58c16febf687717ae7d03244d8233fef1dca
Jacob.Wilde <jacob.wilde@utah.edu>
2020-08-21 19:40:44 -0600

    updated logic

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,17 +1,45 @@
 var questionBank = [
-    {Q:"Inside of which HTML element do we put the JavaScript?", A:2, O1:"<scripting>", O2:"<script>", O3:"<javascript>", O4:"<js>"},
-    {Q:"Where is the correct HTML section to add the Script Element?", A:1, O1:"The <body> section", O2:"The <head> section", O3:"The <header> section", O4:"None of the above"},
-    {Q:"What is the correct syntax for referring to an external script called 'xxx.js'", A:4, O1:'<a href="xxx.js">', O2:'script href="xxx.js"', O3:'<script name="xxx.js"', O4:'<script src="xxx.js"'},
-    {Q:'How do you write "Hello World" in an alert box?', A:3, O1:'msgBox("Hellow World");', O2:'alertBox("Hello World");', O3:'alert("Hello World");', O4:'msg("Hello World");'},
-    {Q:'How do you create a function in JavaScript?', A:2, O1:'function:myFunction()', O2:'function = myFunction()', O3:'function myFunction()', O4:'None of the above'},
+    {
+        Q: "Inside of which HTML element do we put the JavaScript?",
+        A: 1,
+        O: ["<scripting>", "<script>", "<javascript>", "<js>"]
+    },
+    {
+        Q: "Where is the correct HTML section to add the Script Element?",
+        A: 0,
+        O: ["The <body> section", "The <head> section", "The <header> section", "None of the above"]
+    },
+    {
+        Q: "What is the correct syntax for referring to an external script called 'xxx.js'",
+        A: 3,
+        O: ['<a href="xxx.js">', 'script href="xxx.js"', '<script name="xxx.js"', '<script src="xxx.js"']
+    },
+    {
+        Q: 'How do you write "Hello World" in an alert box?',
+        A: 2,
+        O: ['msgBox("Hello World");', 'alertBox("Hello World");', 'alert("Hello World");', 'msg("Hello World");']
+    },
+    {
+        Q: 'How do you create a function in JavaScript?',
+        A: 1,
+        O: ['function:myFunction()', 'function = myFunction()', 'function myFunction()', 'None of the above']
+    }
 ];
-var mainContentEl = document.querySelector("#main-content")
-var h1ContentEl = document.querySelector("#this-content")
-var pContentEl = document.querySelector("#description")
-var p2ContentEl = document.querySelector("#description2")
 
-var systemStartup = function() {
-    
+var currentQuestions = 0
+var correctAnswers = 0
+var incorrectAnswers = 0
+var timeLeft = 10;
+
+var mainContentEl = document.querySelector("#main-content");
+var h1ContentEl = document.querySelector("#this-content");
+var pContentEl = document.querySelector("#description");
+var p2ContentEl = document.querySelector("#description2");
+var olEl = document.querySelector("#my-list");
+var timerEl = document.querySelector("#time-remaining")
+
+var systemStartup = function () {
+
     h1ContentEl.textContent = "Java Coding Quiz Challenge"
     pContentEl.textContent = "Try to answer the following code-related questions within the time limit."
     p2ContentEl.textContent = "Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
@@ -23,16 +51,81 @@ var systemStartup = function() {
     button.appendChild(buttonText);
 
     mainContentEl.appendChild(button);
+    console.log("hello")
 }
+var displayResults = function () {
+    console.log("hello clarice")
+}
+var startTimer = function () {
+    var timeInterval = setInterval(function () {
+
+        if (timeLeft > 0) {
+            timerEl.textContent = "Time Remaining:  " + timeLeft
+            timerEl.className = "top-box"
+            timeLeft--
+        }
+        else {
+            clearInterval(timeInterval);
+            displayResults();
+        }
+    }, 1000);
+}
+var serveQuestion = function (questionNumber) {
+
+    h1ContentEl.textContent = questionBank[questionNumber].Q;
+
+    for (var i = 0; i < 4; i++) {
+
+        var buttonDefault = document.createElement('button');
+        var buttonDefaultText = document.createTextNode((i + 1) + ".  " + questionBank[questionNumber].O[i]);
+        buttonDefault.className = "answer-buttons";
+        buttonDefault.id = i;
+        buttonDefault.appendChild(buttonDefaultText);
+        mainContentEl.appendChild(buttonDefault)
+    }
+}
+
 var startQuiz = function () {
-    h1ContentEl.textContent = "Jahhhhhhhhhhh"
+    startTimer();
+    mainContentEl.removeChild(pContentEl)
+    mainContentEl.removeChild(p2ContentEl)
+    serveQuestion(currentQuestions);
 }
 
 systemStartup();
 
-document.getElementById("startButton").addEventListener("click", function(){
+document.getElementById("startButton").addEventListener("click", function () {
+
+    document.getElementById("startButton").remove()
     startQuiz();
 });
 
+document.addEventListener('click', function (e) {
+    console.log(e)
+    if (e.target.className === "answer-buttons") {
+        var userAnswer = (parseInt(e.target.id));
+        console.log(userAnswer)
+        console.log(questionBank[currentQuestions].A)
+        if (userAnswer === questionBank[currentQuestions].A) {
+            correctAnswers++
+        }
+        else {
+            incorrectAnswers++
+            timeLeft -= 10
+        }
+        console.log(correctAnswers, incorrectAnswers)
+        document.getElementById("0").remove()
+        document.getElementById("1").remove()
+        document.getElementById("2").remove()
+        document.getElementById("3").remove()
 
+        currentQuestions++
 
+        serveQuestion(currentQuestions);
+        // if statement if the answer was correct
+
+        //increment counter
+
+        //serverqustions();
+    }
+})
